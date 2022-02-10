af13a9d47e1ef0a7cbd7836db1ee78d31aeea4b8
Pazhiloi <martinsen06@gmail.com>
2022-02-10 09:17:43 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-"use strict";
+let number = document.getElementById('number')
+let counter = 0
+setInterval(() => {
+  if (counter == 65) {
+    clearInterval()
+  }else{
+    counter += 1
+    number.innerHTML = counter + '%'
+  }
+}, 30);
